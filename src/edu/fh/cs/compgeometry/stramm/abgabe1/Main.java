c2e3968d8dae74adb9f484a2aecbecaa3982b8a7
BastusIII <sebastian.stumpf87@googlemail.com>
2015-04-16 16:03:28 +0200

    push source

---
+++ src/edu/fh/cs/compgeometry/stramm/abgabe1/Main.java
@@ -0,0 +1,15 @@
+package edu.fh.cs.compgeometry.stramm.abgabe1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Basti
+ * Date: 16.04.15
+ * Time: 15:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hoi");
+    }
+}
